5c36f97e1fe6bbd1afff26f79fb5399e82ebb779
VijayaShanthi Gajula <vijayashanthi.gajula@gmail.com>
2025-07-21 16:47:56 +1200

    latest

--- StudendtMSAReact/Context/StudnetDBContext.cs
+++ StudendtMSAReact/Context/StudnetDBContext.cs
@@ -5,16 +5,17 @@ using StudendtMSAReact.Models;//Use
 
 namespace StudendtMSAReact.Context
 {
-    public class StudnetDBContext:DbContext
-        
+    public class StudnetDBContext : DbContext
     {
-        public StudnetDBContext(DbContextOptions<StudnetDBContext> options)
-       : base(options)
+        public StudnetDBContext(DbContextOptions<StudnetDBContext> options) : base(options)
         {
+            Intakes = Set<Intake>();
+            Courses = Set<Course>();
         }
-        //Code for bringing the entity model that we created for intakes and courses
-        public DbSet<Intake> Intakes { get; set; }
-        public DbSet<Course> Courses { get; set; }
+
+        // Code for bringing the entity model that we created for intakes and courses  
+        public DbSet<Intake> Intakes { get; set; } = null!;
+        public DbSet<Course> Courses { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
