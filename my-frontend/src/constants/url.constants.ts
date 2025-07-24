4004c2fabd9e75a5c2bfd13c372778ce69eb1eca
VijayaShanthi Gajula <vijayashanthi.gajula@gmail.com>
2025-07-24 15:00:45 +1200

    dynamic values updated in constants( url calls)

--- my-frontend/src/constants/url.constants.ts
+++ my-frontend/src/constants/url.constants.ts
@@ -1,2 +1,10 @@
-export const baseUrl = "https://localhost:7295/api/Intakes";
-export const CourseUrl = "https://localhost:7295/api/Courses";
+
+
+// export const baseUrl = REACT_APP_BASE_URL+"/Intakes";
+// export const CourseUrl = "https://localhost:7295/api/Courses";
+
+
+const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const baseUrl = `${API_BASE_URL}/Intakes`;
+export const CourseUrl = `${API_BASE_URL}/Courses`;
