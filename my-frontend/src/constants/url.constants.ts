e2d7030be0ecef5f2af86c7a1483ceef4aecf029
VijayaShanthi Gajula <vijayashanthi.gajula@gmail.com>
2025-07-25 01:07:18 +1200

    .

--- my-frontend/src/constants/url.constants.ts
+++ my-frontend/src/constants/url.constants.ts
@@ -1,11 +1,8 @@
-
-
-// export const baseUrl = REACT_APP_BASE_URL+"/Intakes";
-// export const CourseUrl = "https://localhost:7295/api/Courses";
-
+// src/constants/url.constants.ts
+// This file contains the base URLs for the API endpoints used in the application.
 
 const API_BASE_URL = process.env.REACT_APP_BASE_URL;
-
-//export const baseUrl = `${API_BASE_URL}/Intakes`;
-export const baseUrl = "https://msareactbackend-gqagc0e0cchkdzen.eastus-01.azurewebsites.net/api/Intakes";
+export const baseUrl = `${API_BASE_URL}/Intakes`;
+console.log("API_BASE_URL",baseUrl);
 export const CourseUrl =   `${API_BASE_URL}/Courses`;  
+console.log("API_BASE_URL",CourseUrl);
